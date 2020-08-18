038e37fd4a4b21819be460fed68ae19aee55a91e
hoangphung21 <phunghoang211195@gmail.com>
2020-08-17 22:42:42 -0400

    first test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
+
+function RandomBeers() {
+
+  const [name, setName] = useState();
+  const [volumeValue, setVolumeValue] = useState();
+  const [volumeUnit, setVolumeUnit] = useState();
+
+  useEffect(() => {
+    fetch('https://api.punkapi.com/v2/beers/random')
+      .then(results => results.json())
+      .then(data => {
+        setName(data[0].name);
+        const { volume } = data[0];
+        setVolumeValue(volume.value);
+        setVolumeUnit(volume.unit);
+      })
+  }, []);
+
+  return (
+    <div>
+      {name} : {volumeValue}, {volumeUnit}
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <h1>React Beers by Hoang Phung</h1>
+      <RandomBeers />
     </div>
   );
 }
