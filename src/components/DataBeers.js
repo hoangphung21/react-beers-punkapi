75c60a223d73a8836e1d8e206bf9c809779949c8
hoangphung21 <phunghoang211195@gmail.com>
2020-08-19 16:50:31 -0400

    adding description

--- src/components/DataBeers.js
+++ src/components/DataBeers.js
@@ -3,9 +3,14 @@ import axios from 'axios';
 
 function DataBeers() {
     const [posts, setPosts] = useState([]);
+    const [id, setId] = useState(1);
+
+    const handleClick = () => {
+        setId(id)
+    }
 
     useEffect(() => {
-        axios.get('https://api.punkapi.com/v2/beers')
+        axios.get(`https://api.punkapi.com/v2/beers/${id}`)
             .then(res => {
                 console.log(res);
                 setPosts(res.data);
@@ -13,16 +18,24 @@ function DataBeers() {
             .catch(err => {
                 console.log(err);
             });
-    }, []);
+    }, [id]);
 
     return (
         <div>
             <ul>
                 {
-                    posts.map(post => <li key={post.id}>{post.name}</li>)
+                    posts.map(post => <li key={post.id}>{post.id}: {post.name}</li>)
                 }
             </ul>
+            <div>
+                <input type="text" value={id} onChange={e => setId(e.target.value)}></input>
+                <button type="button" onClick={handleClick}>Search</button>
+                <div>{posts.map(post => <li key={post.id}>{post.description}</li>)}</div>
+                <div>{posts.map(post => <img key={post.id} src={post.image_url}></img>)}</div>
+            </div>
         </div>
+
+
     );
 
 }
